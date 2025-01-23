cc4b4ae23c904ac1cd603316e4e2917a681991dd
MLwxlk <alyssonleite25@gmail.com>
2025-01-23 16:09:05 -0300

    Auto-commit: Nova Lista ${newList.name}

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const simpleGit = require('simple-git');
 
 const app = express();
 const PORT = 3000;
+
+// Configura o simple-git
+const git = simpleGit();
 
 // Middleware para interpretar JSON no corpo da requisição
 app.use(express.json());
@@ -17,32 +21,36 @@ app.get('/', (req, res) => {
 });
 
 // Rota POST para salvar os dados no arquivo JSON
-app.post('/save-list', (req, res) => {
+app.post('/save-list', async (req, res) => {
     const newList = req.body; //Dados enviados do formulario
 
     // caminho do arquivo data.json
     const filePath = path.join(__dirname, 'data.json');
 
     // Lê o arquivo atual ou inicializa com um array vazio
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        let lists = [];
-        if (!err && data) {
-            lists = JSON.parse(data); // Converte os dados existentes para um array
-        }
+    try {
+        const data = fs.existsSync(filePath)
+            ? await fs.promises.readFile(filePath, 'utf8')
+            : '[]';
+        const lists = JSON.parse(data);
 
         // adciona a nova lista
         lists.push(newList);
 
         // Salva no arquivo data.json
-        fs.writeFile(filePath, JSON.stringify(lists, null, 2), 'utf8', (writeErr) => {
-            if (writeErr) {
-                console.error('Erro ao salvar o arquivo:', writeErr);
-                return res.status(500).json({ message: 'Erro ao salvar a lista.' });
-            }
+        await fs.promises.writeFile(filePath, JSON.stringify(lists, null, 2), 'utf8');
+        
+        res.status(200).json({ message: 'Lista salva com sucesso!' });
+        
+        // Commit automatico
+        await git.add('./*');
+        await git.commit('Auto-commit: Nova Lista ${newList.name}');
+        await git.push('origin', 'main');
+        console.log('Commit automatico realizado');
 
-            res.status(200).json({ message: 'Lista salva com sucesso!' });
-        });
-    });
+    } catch (err) {
+        console.log('Erro ao salvar a lista:', err);
+    }
 });
 
 // Rota GET para obter as listas do arquivo data.json
